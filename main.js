74c92b7578ba4b691bbb36b312ab982af00d187c
Patrycja Kontek <patrycja.kontek.programista@gmail.com>
2022-05-26 19:14:05 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Witaj przybyszu');
+const name = 'Patrycja Kontek'
+const age = 27;
+console.log(name);
+console.log(age);
+console.log(`${name}
+${age}`);
+
+console.log(`Nazywam się ${name} i mam ${age} lat.`); //literały
